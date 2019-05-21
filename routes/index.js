e0009133c8245ca4278ee8b30e1f87c603b82969
Dkvastolord <adrian.ga_2009@hotmail.com>
2019-05-21 15:56:51 -0500

    card

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+//solicitud a una ruta API REST
+router.get('/hola', function(req, res, next) {
+  res.render('saludo', { title: 'cosa' });
+});
 module.exports = router;
